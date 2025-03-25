f5f4712b769b57b080d372efdb57c48bc7e04cc7
misbahx <misbahx.id@gmail.com>
2025-03-25 13:00:47 +0700

    style: change font style

--- src/shared/ui/provider.tsx
+++ src/shared/ui/provider.tsx
@@ -1,15 +1,23 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        heading: { value: "var(--font-inter)" },
+        body: { value: "var(--font-inter)" },
+      },
+    },
+  },
+});
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
-  )
+  );
 }
